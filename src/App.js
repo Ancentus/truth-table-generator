2e76151a89814bff1e550ffd462e60f112b9ed33
Ancentus <mancentus7@gmail.com>
2023-04-09 16:09:00 +0300

    Generate truth table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const TruthTableGenerator = () => {
+  const [expression, setExpression] = useState("");
+  const [truthTable, setTruthTable] = useState([]);
+
+  const evaluateExpression = (expr, values) => {
+  const result = eval(
+    expr
+      .replace(/[A-Z]/g, (match) => (values[match] ? "true" : "false"))
+      .replace(/\./g, (match) => "&")
+      .replace(/\+/g, (match) => "|")
+      .replace(/!/g, (match) => "!")
+  );
+
+  return result;
+};
+
+const generateTruthTable = () => {
+  const variables = Array.from(new Set(expression.match(/[A-Z]/g))) || [];
+  const numVars = variables.length;
+  const numCombinations = 2 ** numVars;
+  const truthTable = [];
+
+  for (let i = 0; i < numCombinations; i++) {
+    const values = {};
+    const binaryString = i.toString(2).padStart(numVars, "0");
+
+    for (let j = 0; j < numVars; j++) {
+      const varName = variables[j];
+      values[varName] = binaryString.charAt(j) === "1";
+    }
+
+    const result = evaluateExpression(expression, values);
+
+    const row = {
+      values: values,
+      result: result,
+    };
+    truthTable.push(row);
+  }
+
+  setTruthTable(truthTable);
+};
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Truth Table Generator</h2>
+      <input
+        type="text"
+        value={expression}
+        onChange={(e) => setExpression(e.target.value)}
+        placeholder="Enter a digital logic expression"
+      />
+      <button onClick={generateTruthTable}>Generate Truth Table</button>
+      {truthTable.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              {Object.keys(truthTable[0].values).map((variable) => (
+                <th key={variable}>{variable}</th>
+              ))}
+              <th>Output</th>
+            </tr>
+          </thead>
+          <tbody>
+            {truthTable.map((row, index) => (
+              <tr key={index}>
+                {Object.values(row.values).map((value, i) => (
+                  <td key={i}>{value ? "1" : "0"}</td>
+                ))}
+                <td>{row.result ? "1" : "0"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default TruthTableGenerator;
+
